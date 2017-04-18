29e283966c05ee21b36147c7335cc9aacf4d16a9
Sigurd <groneng93@gmail.com>
2017-04-18 07:44:19 -0700

    More tests

--- spec/e2e/tests.spec.js
+++ spec/e2e/tests.spec.js
@@ -8,7 +8,7 @@ describe("End to end tests - ", () => {
   const scoreUrl = baseUrl + "score/";
   const guessUrl = baseUrl + "guess/";
 
-  describe('Play game nicely - ', () => {
+  describe('Happy path game - ', () => {
 
     const drawing = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADg0gClLwABsO71xAAAAABJRU5ErkJggg=="
 
@@ -41,143 +41,145 @@ describe("End to end tests - ", () => {
     })
 
 
+    it('should not have started game before start signal is received', (done) => {
+      fetch(gameUrl + "1/", {
+        method: 'GET'
+      })
+        .then( (rawData) => rawData.json() )
+        .then( (game) => {
+          expect(game.isStarted).toBe(false)
+          done()
+        })
+    })
+
     it('should receive drawing nicely', (done) => {
       fetch(drawingUrl, {
         headers: { 'Content-Type': 'application/json' },
         method: 'POST',
         body: JSON.stringify({ image: drawing, playerId: 0, gamePin: 1, round: 0 })
       })
+      .then( (rawData) => rawData.json() )
+      .then( (data) => {
+        expect(data.status).toBe("success")
+        done()
+      })
+    })
+
+
+    it('should be able to get drawing after posting it', (done) => {
+      fetch(gameUrl + "1/?playerid=1&round=0", {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'GET'
+      })
+      .then( (rawData) => rawData.json() )
+      .then( (data) => {
+        expect(data.image).toEqual(drawing)
+        done()
+      })
+    })
+
+
+    it('should receive second drawing nicely', (done) => {
+      fetch(drawingUrl, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({ image: drawing, playerId: 1, gamePin: 1, round: 0 })
+      })
+      .then( (rawData) => rawData.json() )
+      .then( (data) => {
+        expect(data.status).toBe("success");
+        done()
+      })
+    })
+
+
+    it('should have incremented gameRound when both players have submitte images', (done) => {
+      fetch(gameUrl + "1/", {
+        method: 'GET'
+      })
+      .then( (rawData) => rawData.json() )
+      .then( (data) => {
+        expect(data.round).toBe(1);
+        done();
+      })
+    })
+
+
+    it('should be able to receive guess nicely', (done) => {
+      const payload = { playerId: 1, guess: "banana", gamePin: 1 }
+
+      fetch(guessUrl, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify(payload)
+      })
+      .then( (rawData) => rawData.json() )
+      .then( (data) => {
+        expect(data.status).toBe('success')
+        done()
+      })
+    })
+
+    it('should have put player with playerId 1s guess in ', (done) => {
+      fetch(gameUrl + "1/", {
+        method: 'GET'
+      })
         .then( (rawData) => {
           rawData.json()
-          .then( (data) => {
-            expect(data.status).toBe("success")
-            done();
-          })
-        })
-      })
-
-
-      it('should be able to get drawing after posting it', (done) => {
-        fetch(gameUrl + "1/?playerid=1&round=0", {
-          method: 'GET'
-        })
-          .then( (rawData) => {
-            rawData.json()
             .then( (data) => {
-              expect(data.image).toEqual(drawing)
+              expect(data.guessBlocks[0][1].guessValue).toBe('banana')
               done();
             })
-          })
-      })
-
-
-      it('should receive second drawing nicely', (done) => {
-        fetch(drawingUrl, {
-          headers: { 'Content-Type': 'application/json' },
-          method: 'POST',
-          body: JSON.stringify({ image: drawing, playerId: 1, gamePin: 1, round: 0 })
         })
-          .then( (rawData) => {
-            rawData.json()
+    })
+
+    it('should update scores when they are received', (done) => {
+      const scoreData = {
+        gamePin: 1,
+        scores: {
+          0: 3,
+          1: 1
+        }
+      }
+      fetch(scoreUrl, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify(scoreData)
+      })
+        .then( (rawData) => {
+          rawData.json()
             .then( (data) => {
-              expect(data.status).toBe("success");
+              expect(data.status).toBe("success")
+              expect(data.scores[0]).toBe(3)
+              expect(data.scores[1]).toBe(1)
               done()
             })
-          })
-      })
-
-
-      it('should have incremented gameRound when both players have submitte images', (done) => {
-        fetch(gameUrl + "1/", {
-          method: 'GET'
         })
-          .then( (rawData) => {
-            rawData.json()
+    })
+
+    it('should accumulate scores correctly', (done) => {
+      const secondScoreData = {
+        gamePin: 1,
+        scores: {
+          0: 1,
+          1: 1
+        }
+      }
+      fetch(scoreUrl, {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify(secondScoreData)
+      })
+        .then( (rawData) => {
+          rawData.json()
             .then( (data) => {
-              expect(data.round).toBe(1);
-              done();
+              expect(data.status).toBe("success")
+              expect(data.scores[0]).toBe(4)
+              expect(data.scores[1]).toBe(2)
+              done()
             })
-          })
-      })
-
-
-      it('should be able to receive guess nicely', (done) => {
-        const payload = { playerId: 1, guess: "banana", gamePin: 1 };
-
-        fetch(guessUrl, {
-          headers: { 'Content-Type': 'application/json' },
-          method: 'POST',
-          body: JSON.stringify(payload)
         })
-          .then( (rawData) => {
-            rawData.json()
-              .then( (data) => {
-                expect(data.status).toBe('success');
-                done();
-              })
-          })
-      })
-
-      it('should have put player with playerId 1s guess in ', (done) => {
-        fetch(gameUrl + "1/", {
-          method: 'GET'
-        })
-          .then( (rawData) => {
-            rawData.json()
-              .then( (data) => {
-                expect(data.guessBlocks[0][1].guessValue).toBe('banana');
-                done();
-              });
-          });
-      })
-
-      it('should update scores when they are received', (done) => {
-        const scoreData = {
-          gamePin: 1,
-          scores: {
-            0: 3,
-            1: 1
-          }
-        }
-        fetch(scoreUrl, {
-          headers: { 'Content-Type': 'application/json' },
-          method: 'POST',
-          body: JSON.stringify(scoreData)
-        })
-          .then( (rawData) => {
-            rawData.json()
-              .then( (data) => {
-                expect(data.status).toBe("success");
-                expect(data.scores[0]).toBe(3);
-                expect(data.scores[1]).toBe(1);
-                done();
-              })
-          })
-      })
-
-      it('should accumulate scores correctly', (done) => {
-        const secondScoreData = {
-          gamePin: 1,
-          scores: {
-            0: 1,
-            1: 1
-          }
-        }
-        fetch(scoreUrl, {
-          headers: { 'Content-Type': 'application/json' },
-          method: 'POST',
-          body: JSON.stringify(secondScoreData)
-        })
-          .then( (rawData) => {
-            rawData.json()
-              .then( (data) => {
-                expect(data.status).toBe("success");
-                expect(data.scores[0]).toBe(4);
-                expect(data.scores[1]).toBe(2);
-                done();
-              })
-          });
-      })
+    })
 
   })
 })
